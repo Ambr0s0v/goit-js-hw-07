2f2e5655567284d47addaf0295c57726ab52180e
Ambr0s0v <106374502+Ambr0s0v@users.noreply.github.com>
2023-02-04 13:07:19 +0200

    finish projects

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,29 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+
+function makeGallery(selector){
+    let fullList = galleryItems.map(makeItemMarkup);
+    gallery.append(...fullList);   
+}
+
+function makeItemMarkup({preview,original,description} = {}){
+    let galleryItem = document.createElement("a");
+    galleryItem.classList.add("galley__link");
+    galleryItem.setAttribute("href",original);
+    galleryItem.insertAdjacentHTML("afterbegin",`<img class="gallery__image" src="${preview}" alt="${description}" />`);
+  return galleryItem;  
+}
+
+function createLightbox(){
+    return new SimpleLightbox(".gallery a",{
+        captionDelay: 250,
+        captionsData:"alt",
+        scrollZoom:false,   
+    });
+}
+
+
+makeGallery(gallery);
+createLightbox();
